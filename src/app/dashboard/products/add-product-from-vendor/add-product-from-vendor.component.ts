4f8ad09c26140c7658802f446edc4ff83b7840cc
andrey-kirillov <andrey.kirillov.kiev@gmail.com>
2018-03-14 19:01:21 +0200

    form Add Product

--- src/app/dashboard/products/add-product-from-vendor/add-product-from-vendor.component.ts
+++ src/app/dashboard/products/add-product-from-vendor/add-product-from-vendor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-add-product-from-vendor',
   templateUrl: './add-product-from-vendor.component.html',
@@ -13,6 +14,25 @@ export class AddProductFromVendorComponent implements OnInit {
   public selectAll: boolean;
   public item: any = {};
   public variants: any = [{},{}];
+
+  //all variables after this comment are only for test
+  currentVariant={
+    custom_attr:[]
+  }
+  variationArrs = {
+    package_type: ['one', 'two'],
+    unit_type: ['one', 'two'],
+    units_per_package: ['one', 'two'],
+    size: ['one', 'two'],
+    material: ['one', 'two'],
+    price_range: ['one', 'two']
+  };
+  variation={
+    ackage_type:''
+  }
+  departmentCollection=['one', 'two'];
+  productCategoriesCollection=['one', 'two'];
+  productAccountingCollection=['one', 'two'];
 
   constructor() { }
 
--- src/app/dashboard/products/add-product-from-vendor/add-product-from-vendor.module.ts
+++ src/app/dashboard/products/add-product-from-vendor/add-product-from-vendor.module.ts
@@ -3,10 +3,14 @@ import {NgModule} from "@angular/core";
 import {AddProductFromVendorComponent} from "./add-product-from-vendor.component";
 import {ProductVariantComponent} from "./product-variant/product-variant.component";
 import { AppSharedModule } from '../../../shared/shared.module';
+
+
+
+
 @NgModule({
   declarations: [
     AddProductFromVendorComponent,
-    ProductVariantComponent
+    ProductVariantComponent,
   ],
   exports:[
     AddProductFromVendorComponent
