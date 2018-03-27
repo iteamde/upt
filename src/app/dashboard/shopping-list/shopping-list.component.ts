4428dfa84e9f04f06d081bebdcbbcafa8951c8a4
andrey-kirillov <andrey.kirillov.kiev@gmail.com>
2018-03-27 09:58:49 +0300

    refact auto-price -search

--- src/app/dashboard/shopping-list/shopping-list.component.ts
+++ src/app/dashboard/shopping-list/shopping-list.component.ts
@@ -68,9 +68,15 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
         (f.location == '' || f.location == item.location_name)
         && (f.vendor == '' || f.vendor == item.selected_vendor.vendor_name))
         && (!f.onlymy || this.userService.selfData.id == item.created_by)
-        && (((item.price ? item.price : parseFloat(item.price_lower.slice(1))) >= f.someRange[0])
-          && (((item.price ? item.price : parseFloat(item.price_upper.slice(1))) <= f.someRange[1])))
-        //&& (( parseFloat(item.price_lower.substring(1)) >= f.someRange[0] ) && (parseFloat(item.price_upper.substring(1)) <= f.someRange[1]))
+        && (item.price >0 ? ((item.price >= f.someRange[0]) && (item.price <= f.someRange[1])) :
+            (((parseFloat(item.price_lower.slice(1))>= f.someRange[0]) && (parseFloat(item.price_lower.slice(1)) <= f.someRange[1])) ||
+              ((parseFloat(item.price_upper.slice(1))>= f.someRange[0]) && (parseFloat(item.price_upper.slice(1)) <= f.someRange[1]))
+            ))
+
+
+        //&& (((item.price ? item.price : parseFloat(item.price_lower.slice(1))) >= f.someRange[0])
+        //  && (((item.price ? item.price : parseFloat(item.price_upper.slice(1))) <= f.someRange[1])))
+
       );
 
 
